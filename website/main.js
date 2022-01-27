3b488d9e241089f8618c76652d6ab36c4b9d358f
OGB123 <86609011+OGB123@users.noreply.github.com>
2022-01-27 12:26:27 +0000

    Create main.js

---
+++ website/main.js
@@ -0,0 +1,8 @@
+function hideLoader() {
+  $('#loading').hide();
+}
+
+$(window).ready(hideLoader);
+
+// Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
+setTimeout(hideLoader, 20 * 1000);
