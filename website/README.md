40c9da763ba085b86098f0ffbf96188821d4f246
ОГБ <86609011+T0dCNg@users.noreply.github.com>
2022-02-21 23:57:07 +0000

    Delete website directory

--- website/README.md
+++
@@ -1,5 +0,0 @@
-**AIO website**
-
-By OGB and Imra Kiss
-
-The is the cource code for the all in one website 
--- website/main.js
+++
@@ -1,8 +0,0 @@
-function hideLoader() {
-  $('#loading').hide();
-}
-
-$(window).ready(hideLoader);
-
-// Strongly recommended: Hide loader after 20 seconds, even if the page hasn't finished loading
-setTimeout(hideLoader, 20 * 1000);
