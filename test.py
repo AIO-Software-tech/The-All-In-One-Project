c4d20cd3f12ebbd0b000dbb69c2620262922ca6d
ОГБ <86609011+T0dCNg@users.noreply.github.com>
2022-04-28 23:50:00 +0100

    Update to test 1.2

--- test.py
+++ test.py
@@ -157,45 +157,92 @@ del temp
 
 # Play Game
 
-print_board(board_display)
+howManyPlayers = int(input("How many people are playing (1-2): "))
 
-for turn in range(num_turns):
-  print("Turn:", turn + 1, "of", num_turns)
-  print("Ships left:", len(ship_list))
-  print()
-  
-  guess_coords = {}
-  while True:
-    guess_coords['row'] = get_row()
-    guess_coords['col'] = get_col()
-    if board_display[guess_coords['row']][guess_coords['col']] == 'X' or \
-     board_display[guess_coords['row']][guess_coords['col']] == '*':
-      print("\nYou guessed that one already.")
-    else:
-      break
-
-  
-  ship_hit = False
-  for ship in ship_list:
-    if ship.contains(guess_coords):
-      print("Hit!")
-      ship_hit = True
-      board_display[guess_coords['row']][guess_coords['col']] = 'X'
-      if ship.destroyed():
-        print("Ship Destroyed!")
-        ship_list.remove(ship)
-      break
-  if not ship_hit:
-    board_display[guess_coords['row']][guess_coords['col']] = '*'
-    print("You missed!")
-
+if howManyPlayers == 1:
   print_board(board_display)
-  
-  if not ship_list:
-    break
 
-# End Game
-if ship_list:
-  print("You lose!")
-else:
-  print("All the ships are sunk. You win!")
+  for turn in range(num_turns):
+    print("Turn:", turn + 1, "of", num_turns)
+    print("Ships left:", len(ship_list))
+    print()
+
+    guess_coords = {}
+    while True:
+      guess_coords['row'] = get_row()
+      guess_coords['col'] = get_col()
+      if board_display[guess_coords['row']][guess_coords['col']] == 'X' or \
+              board_display[guess_coords['row']][guess_coords['col']] == '*':
+        print("\nYou guessed that one already.")
+      else:
+        break
+
+    ship_hit = False
+    for ship in ship_list:
+      if ship.contains(guess_coords):
+        print("Hit!")
+        ship_hit = True
+        board_display[guess_coords['row']][guess_coords['col']] = 'X'
+        if ship.destroyed():
+          print("Ship Destroyed!")
+          ship_list.remove(ship)
+        break
+    if not ship_hit:
+      board_display[guess_coords['row']][guess_coords['col']] = '*'
+      print("You missed!")
+
+    print_board(board_display)
+
+    if not ship_list:
+      break
+
+  # End Game
+  if ship_list:
+    print("You lose!")
+  else:
+    print("All the ships are sunk. You win!")
+
+elif howManyPlayers == 2:
+  def multiPlayer():
+    print("Player 1's turn")
+    print_board(board_display)
+
+    for turn in range(num_turns):
+      print("Turn:", turn + 1, "of", num_turns)
+      print("Ships left:", len(ship_list))
+      print()
+
+      guess_coords = {}
+      while True:
+        guess_coords['row'] = get_row()
+        guess_coords['col'] = get_col()
+        if board_display[guess_coords['row']][guess_coords['col']] == 'X' or \
+                board_display[guess_coords['row']][guess_coords['col']] == '*':
+          print("\nYou guessed that one already.")
+        else:
+          break
+
+      ship_hit = False
+      for ship in ship_list:
+        if ship.contains(guess_coords):
+          print("Hit!")
+          ship_hit = True
+          board_display[guess_coords['row']][guess_coords['col']] = 'X'
+          if ship.destroyed():
+            print("Ship Destroyed!")
+            ship_list.remove(ship)
+          break
+      if not ship_hit:
+        board_display[guess_coords['row']][guess_coords['col']] = '*'
+        print("You missed!")
+
+      print_board(board_display)
+
+      if not ship_list:
+        break
+
+    # End Game
+    if ship_list:
+      print("You lose!")
+    else:
+      print("All the ships are sunk. You win!")
