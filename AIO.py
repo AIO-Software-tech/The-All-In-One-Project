9084b032dac07ffecafac0478c998e8682c89966
ОГБ <86609011+T0dCNg@users.noreply.github.com>
2022-05-09 09:35:10 +0100

    Update 1.6.0

--- AIO.py
+++ AIO.py
@@ -84,7 +84,7 @@ def menu():
 
             if CalculationMenu == 5:
 
-    # Calculator: Riding System
+         # Calculator: Riding System
 
 
                     ride = 0
@@ -283,7 +283,7 @@ def menu():
             print("│                     │")
             print("│  2 = Coin Flip      │")
             print("│                     │")
-            print("│  3 = Blank          │")
+            print("│  3 = Connect 4      │")
             print("│                     │")
             print("│  4 = Blank          │")
             print("│                     │")
@@ -502,7 +502,7 @@ def menu():
                 # Play Game
                 play_game()
 
-            elif gameSelecte == 2:
+            elif gameSelecte == 2:-
                 
                 # Coin Flip:
 
@@ -548,9 +548,118 @@ def menu():
                         menu()
 
                 Game()
-            else:
-                print("Error core: 1")
-                menu()
+            elif gameSelecte == 3:
+                board = [] #List for holding the board
+
+                for x in range(6):
+                    board.append(["O"] * 7) #builds 7 x 6 board (rows x columns)
+
+                #function for printing the board
+                def print_board(board):  
+                    for row in board:
+                        print (" ".join(row))
+
+                print ('Welcome to Connect Four')
+
+                player_one = input('Player 1. Enter your name: ')
+                player_two = input('Player 2. Enter your name: ') #Gets players names
+
+                print ('%s vs. %s' % (player_one, player_two))
+                print ('--------------')
+                print (print_board(board))
+                print ('Player 1 is @ and Player 2 is #')
+                print ('Let\'s play!!') # Game's 'Opening'
+
+                turn = 0 #Keeps track of turn
+
+
+                while turn < 8: #debugging purposes
+
+                    if turn % 2 == 0: #Determines whose turn it is by checking for even or odd turn 
+
+                        print ('%s. Choose a column to drop your chip' % (player_one))
+                        one_choice = int(input('Column: ')) #Determines what column player will drop chip
+
+                    #Checks for 'empty slot' from bottom up and fills it with players 'chip'
+
+                        if (board[5][one_choice - 1] == 'O'):             
+                            board[5][one_choice - 1] = '@'
+                            print_board(board)
+                            turn += 1
+
+                        elif(board[4][one_choice - 1] == 'O'):
+                            board[4][one_choice - 1] = '@'
+                            print_board(board)
+                            turn += 1
+
+                        elif(board[3][one_choice - 1] == 'O'):
+                            board[3][one_choice - 1] = '@'
+                            print_board(board)
+                            turn += 1
+
+                        elif (board[2][one_choice - 1] == 'O'):
+                            board[2][one_choice - 1] = '@'
+                            print_board(board)
+                            turn += 1
+
+                        elif (board[1][one_choice - 1] == 'O'):
+                            board[1][one_choice - 1] = '@'
+                            print_board(board)
+                            turn += 1
+
+                        elif (board[0][one_choice - 1] == 'O'):
+                            board[0][one_choice - 1] = '@'
+                            print_board(board)
+                            turn += 1
+
+                        else:
+                            print ("Column is full!!")      
+                    else:         
+
+                        #Same as above for player 2
+                        print ('%s. Choose a column to drop your chip' % (player_two))
+                        one_choice = int(input('Column: '))
+
+                        if (board[5][one_choice - 1] == 'O'):
+                            board[5][one_choice - 1] = '#'
+                            print_board(board)
+                            turn += 1
+
+                        elif(board[4][one_choice - 1] == 'O'):
+                            board[4][one_choice - 1] = '#'
+                            print_board(board)
+                            turn += 1
+
+                        elif(board[3][one_choice - 1] == 'O'):
+                            board[3][one_choice - 1] = '#'
+                            print_board(board)
+                            turn += 1
+
+                        elif (board[2][one_choice - 1] == 'O'):
+                            board[2][one_choice - 1] = '#'
+                            print_board(board)
+                            turn += 1
+
+                        elif (board[1][one_choice - 1] == 'O'):
+                            board[1][one_choice - 1] = '#'
+                            print_board(board)
+                            turn += 1
+
+                        elif (board[0][one_choice - 1] == 'O'):
+                            board[0][one_choice - 1] = '#'
+                            print_board(board)
+                            turn += 1
+
+                        else:
+                            print ("Column is full!!")
+
+                #Gets me out of loop for debugging
+                if turn == 8:
+                    print ('Game Over man!')
+
+                        else:
+                            print("Error core: 1")
+                            menu()
 
         # Errors
         elif unit == 4:
