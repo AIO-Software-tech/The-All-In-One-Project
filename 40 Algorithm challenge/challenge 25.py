1a429124991370769d739adcc9ffe09167aa96fd
ОГБ <86609011+T0dCNg@users.noreply.github.com>
2022-06-06 14:09:43 +0000

    Update finally

---
+++ 40 Algorithm challenge/challenge 25.py
@@ -0,0 +1,15 @@
+print("Age catagory= Under 18 = U18")
+print("Age catagory= 62 or Older = Snr")
+print("Age catagory= If your a veteran = Vet")
+
+print( )
+
+AgeCat = input("Please enter your age catagory: ")
+if AgeCat == "U18":
+    print("You are in: Speedy Club")
+elif AgeCat == "Vet":
+    print("You are in: Veteran Club")
+elif AgeCat == "Snr":
+    print("You are in: Lightening fast runner")
+else:
+  print("lol noob")
