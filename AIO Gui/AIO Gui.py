db005f585030cb68d9ce04ff36bf3a33712b51f9
ОГБ <86609011+T0dCNg@users.noreply.github.com>
2022-08-11 22:37:58 +0100

    AIO GUI - v0.0.2 - Alpha - Rev 1

--- AIO Gui/AIO Gui.py
+++ AIO Gui/AIO Gui.py
@@ -2,14 +2,15 @@ import sys
 from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QDialog, QApplication
 from PyQt5.uic import loadUi
+from datetime import date
 
 
-#Login System
+#!Login System
 class Login(QDialog):
     def __init__(self):
         
         super(Login, self).__init__()
-        loadUi("Aio Gui Login.ui",self)
+        loadUi("AIO Gui Login.ui",self)
         self.Login.clicked.connect(self.loginfunction)
         self.Password.setEchoMode(QtWidgets.QLineEdit.Password)
         self.Close.clicked.connect(QtWidgets.qApp.quit)
@@ -28,8 +29,18 @@ class Login(QDialog):
             widget.setCurrentIndex(widget.currentIndex()+1)
         else:
             widget.setCurrentIndex(widget.currentIndex()+2)
+  
+#!Incorect Username Or Password
+class IccorectUserOrPass(QDialog):
+    def __init__(self):
+        super(IccorectUserOrPass, self).__init__()
+        loadUi("IUOP.ui", self)
+        self.Ok.clicked.connect(self.windowChange)
 
-#The Main Menu
+    def windowChange(self):
+        widget.setCurrentIndex(widget.currentIndex()-2)
+
+#?The Main Menu
 class MainMenu(QDialog):
     def __init__(self):
         super(MainMenu, self).__init__()
@@ -52,19 +63,7 @@ class MainMenu(QDialog):
     def ChangeToGames(self):
         widget.setCurrentIndex(widget.currentIndex()+5)
 
-        
-        
-#Incorect Username Or Password
-class IccorectUserOrPass(QDialog):
-    def __init__(self):
-        super(IccorectUserOrPass, self).__init__()
-        loadUi("IUOP.ui", self)
-        self.Ok.clicked.connect(self.windowChange)
-
-    def windowChange(self):
-        widget.setCurrentIndex(widget.currentIndex()-2)
-
-#Error Codes Menu
+#?Error Codes Menu
 class ErrorCodes(QDialog):
     def __init__(self):
         super(ErrorCodes, self).__init__()
@@ -74,12 +73,14 @@ class ErrorCodes(QDialog):
     def windowChange(self):
         widget.setCurrentIndex(widget.currentIndex()-2)
 
-#Personal Menus
+#?Personal Menus
 class Personal(QDialog):
     def __init__(self):
         super(Personal, self).__init__()
         loadUi("Personal Menu.ui", self)
         self.Name.clicked.connect(self.ChangeToName)
+        self.Age.clicked.connect(self.ChangeToAge)
+        self.Address.clicked.connect(self.ChangeToAddress)
         self.Close.clicked.connect(self.windowChange)
 
     def windowChange(self):
@@ -88,25 +89,63 @@ class Personal(QDialog):
     def ChangeToName(self):
         widget.setCurrentIndex(widget.currentIndex()+1)
 
+    def ChangeToAge(self):
+        widget.setCurrentIndex(widget.currentIndex()+2)
+
+    def ChangeToAddress(self):
+        widget.setCurrentIndex(widget.currentIndex()+3)
+
+#*Personal Name Menu
 class PersonalNameMenu(QDialog):
     def __init__(self):
         super(PersonalNameMenu, self).__init__()
         loadUi("PersonalNameMenu.ui", self)
-        self.Submit.clicked.connect(self.NameOutput)
+        self.Submit.clicked.connect(self.OutputName)
         self.Close.clicked.connect(self.windowChange)
 
     def windowChange(self):
         widget.setCurrentIndex(widget.currentIndex()-1)
 
-    def NameOutput(self):
+    def OutputName(self):
         FirstName=self.Forname.text()
         LastName=self.Surname.text()
         print(FirstName + LastName)
-        Name = [{"Firstname":"test","Lastname":"Tester"}]
-        row=0
-        for Name in Name:
-            self.tableWidget.setItem(row, 0, QtWidgets.QTableWidgetItem(Name["Firstname"]))
+        self.Output.setText(FirstName + " " + LastName)
 
+#*Personal Age Menu
+class PersonalAgeMenu(QDialog):
+    def __init__(self):
+        super(PersonalAgeMenu, self).__init__()
+        loadUi("PersonalAgeMenu.ui", self)#
+        self.Submit.clicked.connect(self.OutputAge)
+        self.Close.clicked.connect(self.windowChange)
+
+    def windowChange(self):
+        widget.setCurrentIndex(widget.currentIndex()-2)
+
+    def OutputAge(self):
+        YourAge=self.YourAge.text()
+        current_year = date.today().year
+        Output = int(current_year) - int(YourAge)
+        self.Output.setText(str(Output))
+
+#*Personal Address Menu
+class PersonalAddressMenu(QDialog):
+    def __init__(self):
+        super(PersonalAddressMenu, self).__init__()
+        loadUi("PersonalAddressMenu.ui", self)
+        self.Submit.clicked.connect(self.OutputAddress)
+        self.Close.clicked.connect(self.windowChange)
+
+    def windowChange(self):
+        widget.setCurrentIndex(widget.currentIndex()-3)
+
+    def OutputAddress(self):
+        First4 = self.First4.text()
+        Last3 = self.Last3.text()
+        self.Output.setText(First4 + " " + Last3)
+
+#?Caculations Menu
 class Caculations(QDialog):
     def __init__(self):
         super(Caculations, self).__init__()
@@ -116,6 +155,7 @@ class Caculations(QDialog):
     def windowChange(self):
         widget.setCurrentIndex(widget.currentIndex()-4)
 
+#?Game Menu
 class Games(QDialog):
     def __init__(self):
         super(Games, self).__init__()
@@ -135,6 +175,8 @@ IccorectUserOrPass = IccorectUserOrPass()
 AIOErrorCodes = ErrorCodes()
 Personal = Personal()
 PersonalNameMenu = PersonalNameMenu()
+PersonalAgeMenu = PersonalAgeMenu()
+PersonalAddressMenu = PersonalAddressMenu()
 Caculations = Caculations()
 Games = Games()
 
@@ -146,6 +188,8 @@ widget.addWidget(IccorectUserOrPass)
 widget.addWidget(AIOErrorCodes)
 widget.addWidget(Personal)
 widget.addWidget(PersonalNameMenu)
+widget.addWidget(PersonalAgeMenu)
+widget.addWidget(PersonalAddressMenu)
 widget.addWidget(Caculations)
 widget.addWidget(Games)
 
