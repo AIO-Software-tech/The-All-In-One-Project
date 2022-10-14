a1233f9b388094047326a92de43f59f7e7d89018
ОГБ <86609011+T0dCNg@users.noreply.github.com>
2022-10-15 00:45:56 +0100

    AIO V-0.0.1 Rev-1 Alpha

---
+++ AIO In Go/main.go
@@ -0,0 +1,11 @@
+package main
+import "fmt"
+
+func main() {
+
+	fmt.print("All-In-One By Imre Kiss And Oliver Boucher")
+	fmt.print(" ")
+	fmt.print("Verstion 0.0.1 REV-1 2022")
+	fmt.print(" ")
+
+}
