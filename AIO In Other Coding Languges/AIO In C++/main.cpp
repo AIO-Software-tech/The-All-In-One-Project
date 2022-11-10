39c29847e0e998d7bec5182ab90df5ab67fa8a5a
unknown <Ollie@Ollies-Pc.boucher.local>
2022-11-10 19:45:14 +0000

    AIO Launcher V-0.1.2 Rev-1
    Signed-off-by: unknown <Ollie@Ollies-Pc.boucher.local>

--- AIO In Other Coding Languges/AIO In C++/main.cpp
+++ AIO In Other Coding Languges/AIO In C++/main.cpp
@@ -3,7 +3,8 @@ using namespace std;
 
 int main() {
     cout << "Hello World\n\n";
-    cout << "I am learning C++\n\n";
+    cout << "I am learnin123g C++\n\n";
     string hello;
+    system("pause");
     return 0;
 }
